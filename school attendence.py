7c0171e63b415a5dba89ab5ff1543113596eb0b7
dhanvanthrajinu515-coder <dhanvanthrajinu515@gmail.com>
2026-05-30 21:47:26 +0530

    Add files via upload

---
+++ school attendence.py
@@ -0,0 +1,355 @@
+import datetime as dt
+class students:
+
+    def dhanvanth_rajinu(self):
+
+        a=input("dhanvanth rajinu P or A: ")
+        print("DHANVANTH RAJINU attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"dhanvanth rajinu- {a}\n")
+        x.close()
+
+        x=open("attendence.txt","a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+
+    def vishva(self):
+
+        a=input("vishva P or A: ")
+        print("VISHVA attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"vishva- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def akhil(self):
+
+        a=input("akhil P or A")
+        print("AKHIL attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"akhil- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def hari(self):
+
+        a=input("hari P or A: ")
+        print("HARI attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"hari- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def surya(self):
+
+        a=input("surya P or A: ")
+        print("SURYA attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"surya- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def gokul(self):
+
+        a=input("gokul P or A: ")
+        print("GOKUL attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"gokul- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def dhyanesh(self):
+
+        a=input("dhyanesh P or A")
+        print("DHYANESH attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"dhyanesh- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+class teachers:
+
+    def gokila(self):
+        a=input("gokila P or A: ")
+        print("GOGILA attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"gokila- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def uma(self):
+
+        a=input("uma P or A: ")
+        print("UMA attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"uma- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def prabha(self):
+
+        a=input("prabha P or A: ")
+        print("PRABHA attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"prabha- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n")
+        x.close()
+
+    def velumani(self):
+        a=input("velumani P or A: ")
+        print("VELUMANI attendence added\n")
+        x=open("attendence.txt","a")
+        x.write(f"velumani- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def anushiya(self):
+
+        a=input("anushiya P or A: ")
+        print("ANUSHIYA attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"anushiya- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def umar_sherif(self):
+
+        a=input("umar_sherif P or A: ")
+        print("OMAR SHERIF attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"umar_sherif- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+class workers:
+
+    def palaniammal(self):
+        a=input("palaniammal P or A: ")
+        print("PALANIAMMAL attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"palaniammal- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def velammal(self):
+
+        a=input("velammal P or A: ")
+        print("VELAMMAL attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"velammal- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def x(self):
+        a=input("x P or A: ")
+        print("X attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"x- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def y(self):
+        a=input("y P or A: ")
+        print("Y attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"y- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+class drivers:
+
+    def arumugam(self):
+        a=input("arumugamP P or A: ")
+        print("ARUMUGAM attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"arumugam- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def velavan(self):
+        a=input("velavan P or A: ")
+        print("VELAVAN attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"velavan- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+    def ayyapan(self):
+        a=input("ayyapan P or A: ")
+        print("AYYAPPAN attendence added\n")
+
+        x=open("attendence.txt","a")
+        x.write(f"ayyapan- {a}\n")
+        x.close()
+
+        x = open("attendence.txt", "a")
+        x.write(f"{dt.datetime.now()}\n\n")
+        x.close()
+
+obj=students()
+obj1=teachers()
+obj2=workers()
+obj3=drivers()
+
+while True:
+    print("----students----")
+    print("1.dhanvanth")
+    print("2.vishva")
+    print("3.akhil")
+    print("4.hari")
+    print("5.surya")
+    print("6.gokul")
+    print("7.dhyanesh\n")
+    print("----teachers----")
+    print("8. gokila")
+    print("9.uma")
+    print("10.prabha")
+    print("11.velumani")
+    print("12.anushiya")
+    print("13.omar_sherif\n")
+    print("----workers----")
+    print("14.palaniammal")
+    print("15.velammal")
+    print("16.x")
+    print("17.y\n")
+    print("----drivers----")
+    print("18.arumugam")
+    print("19.velavan")
+    print("20.ayyapan\n")
+
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        obj.dhanvanth_rajinu()
+
+    elif choice == 2:
+        obj.vishva()
+
+    elif choice == 3:
+        obj.akhil()
+
+    elif choice == 4:
+        obj.hari()
+
+    elif choice == 5:
+        obj.surya()
+
+    elif choice == 6:
+        obj.gokul()
+
+    elif choice == 7:
+        obj.dhyanesh()
+
+    elif choice == 8:
+        obj1.gokila()
+
+    elif choice == 9:
+        obj1.uma()
+
+    elif choice == 10:
+        obj1.prabha()
+
+    elif choice == 11:
+        obj1.velumani()
+
+    elif choice == 12:
+        obj1.anushiya()
+
+    elif choice == 13:
+        obj1.omar_sherif()
+
+    elif choice == 14:
+        obj2.palaniammal()
+
+    elif choice == 15:
+        obj2.velammal()
+
+    elif choice == 16:
+        obj2.x()
+
+    elif choice == 17:
+        obj2.y()
+
+    elif choice == 18:
+        obj3.arumugam()
+
+    elif choice == 19:
+        obj3.velavan()
+
+    elif choice == 20:
+        obj3.ayyapan()
+
+    else:
+        print("Enter a valid choice")
